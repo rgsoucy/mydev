4d97571b1cc7a535b1fffd950b2d9aec42946308
Ryan <rgsoucy@umass.edu>
2016-10-03 19:56:11 -0400

    ngTest1 edits for angular class

--- angular/ngTest1/scripts/controller.js
+++ angular/ngTest1/scripts/controller.js
@@ -1,9 +1,17 @@
 angular
     .module("myApp")
     .controller("myController", ['$scope', '$log', '$filter', '$http', function ($scope, $log, $filter, $http) {
-
-        $log.info("Hello World!");
+        $scope.myVar = '';
         $scope.name = 'Ryan';
+        $scope.makeUpper = function () {
+            return $filter('uppercase')($scope.myVar);
+        };
 
+        //watcher function
+        $scope.$watch('myVar', function (newVal, oldVal) {
+            $log.info("Changed!");
+            $log.log('Old: ' + oldVal);
+            $log.log('New: ' + newVal);
+        });
 
     }]);
